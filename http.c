6c6558e8a934be3e1d1d8811e5fd5cfe4aa3d9ca
demindx <oliver9574@gmail.com>
2025-12-31 22:34:39 +0100

    feat header rework

--- http.c
+++ http.c
@@ -1,161 +1,120 @@
-#include "http.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
+#include "http.h"
+#include "status.h"
 
-Header *createHeader(char *contentType, int contentLength) {
-    Header *header = malloc(sizeof(Header));
-    if (header == NULL) {
-        return NULL;
-    }
+#define BUFF_SIZE 8192
 
-    header->contentType = strdup(contentType);
-    if (header->contentType == NULL) {
-        free(header);
-        return NULL;
-    }
+char *methods[] = {"GET",  "POST",    "PUT",     "PATCH", "DELETE",
+                   "HEAD", "OPTIONS", "CONNECT", "TRACE"};
 
-    header->contentLenght = contentLength;
+struct Header {
+  char *name;
+  char *value;
 
-    return header;
-}
+  Header *next;
+  Header *prev;
+};
 
-ResponseStartLine *createResponseStartLine(char *version, HttpStatus status) {
-    ResponseStartLine *startLine = malloc(sizeof(ResponseStartLine));
-    if (startLine == NULL) {
-        return NULL;
-    }
+struct Request {
+  HttpMethod method;
+  char *uri;
+  char *version;
 
-    char buff[100];
-    sprintf(buff, "HTTP/%s", version);
+  Header *header;
 
-    startLine->version = strdup(buff);
-    if (startLine->version == NULL) {
-        free(startLine);
-        return NULL;
-    }
+  char *body;
+};
 
-    startLine->status = status;
+struct Response {
+  char *version;
+  HttpStatus status;
 
-    return startLine;
-}
+  char *body;
+};
 
-Response *createReponse(char *contentType, char *body, HttpStatus status) {
-    Response *response = malloc(sizeof(Response));
-    if (response == NULL) {
-        return NULL;
-    }
+Header *createHeader(const char *name, const char *value) {
+  Header *header = malloc(sizeof(Header));
+  if (header == NULL) {
+    return NULL;
+  }
 
-    response->body = strdup(body);
-
-    if(response->body == NULL) {
-        free(response);
-        return NULL;
-    }
-
-    ResponseStartLine *startLine = createResponseStartLine("1.1", status);
-    if (startLine == NULL) {
-        freeResponse(response);
-        return NULL;
-    } 
-
-    Header *header = createHeader(contentType, strlen(body));
-    if (header == NULL) {
-        freeResponse(response);
-        freeResponseStartLine(startLine);
-        return NULL;
-    }
-
-    response->header = header;
-    response->startLine = startLine;
-
-    return response;
-}
-
-char *serializeResponseStartLine(ResponseStartLine startLine) {
-    char buff[256];
-    char *statusMsg = "";
-
-    switch (startLine.status) {
-        case OK_200:
-            statusMsg = "OK";
-        break;
-    }
-
-    sprintf(buff, "%s %d %s\r\n", startLine.version, startLine.status, statusMsg);
-
-    char *result = strdup(buff);
-    if (result == NULL) {
-        return NULL;
-    }
-
-    return result;
-}
-
-char *serializeHeader(Header header) {
-    char buff[512];
-
-    sprintf(buff, "Content-Type: %s\r\n" 
-            "Content-Length: %d\r\n",
-            header.contentType,
-            header.contentLenght);
-
-    char *result = strdup(buff);
-    if (result == NULL) {
-        return NULL;
-    }
-
-    return result;
-}
-
-char *serializeResponse(Response response) {
-    char *startLine = serializeResponseStartLine(*response.startLine);
-    if (startLine == NULL) {
-        return NULL;
-    }
-
-    char *header = serializeHeader(*response.header);
-    if (header == NULL) {
-        free(startLine);
-        return NULL;
-    }
-
-    int len = sizeof(char) *(strlen(startLine) + strlen(header) + (response.body ? strlen(response.body) : 0) + 1);
-
-    char *result = malloc(len);
-    if (result == NULL) {
-        free(header);
-        free(startLine);
-        return NULL;
-    }
-
-    strcpy(result, startLine);
-    strcat(result, header);
-
-    if (response.body) {
-        strcat(result, "\r\n");
-        strcat(result, response.body);
-    }
-
+  header->name = strdup(name);
+  if (header->name == NULL) {
     free(header);
-    free(startLine);
-    return result;
-}
+    return NULL;
+  }
 
-void freeResponseStartLine(ResponseStartLine *startLine) {
-    free(startLine->version);
-    free(startLine);
-}
-
-void freeResponse(Response *response) {
-    freeResponseStartLine(response->startLine);
-    freeHeader(response->header);
-    free(response->body);
-    free(response);
-}
-
-void freeHeader(Header *header) {
-    free(header->contentType);
+  header->value = strdup(value);
+  if (header->value == NULL) {
+    free(header->name);
     free(header);
+    return NULL;
+  }
+
+  header->next = NULL;
+  header->prev = NULL;
+
+  return header;
+}
+
+void addHeader(Header *head, Header *header) {
+  if (head == NULL || header == NULL) {
+    return;
+  }
+
+  Header *ptr = head;
+  while (ptr->next != NULL) {
+    ptr = ptr->next;
+  }
+
+  ptr->next = header;
+}
+
+void freeHeader(Header *head) {
+  if (head == NULL) {
+    return;
+  }
+
+  head->next = NULL;
+  head->prev = NULL;
+  free(head->name);
+  free(head->value);
+
+  free(head);
+}
+
+Header *readHeader(int sock) {
+  // TODO: read header
+
+  return NULL;
+}
+
+Request *readRequest(int sock) {
+  Request *request = malloc(sizeof(Request));
+  if (request == NULL) {
+    return NULL;
+  }
+
+  request->header = readHeader(sock);
+
+  return request;
+}
+
+int parseHttpMethod(char *buff) {
+  for (int i = 0; i < METHODS_LENGTH; ++i) {
+    if (strcmp(buff, methods[i]) == 0) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+int parseRequestLine(int sock, Request *request) {
+  // TODO: parse request line
+
+  return -1;
 }
